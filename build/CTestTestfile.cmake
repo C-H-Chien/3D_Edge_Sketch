140d0058d1a294c7935c1bcd417bee9aab4daf8f
Chiang-Heng Chien <chiang-heng_chien@brown.edu>
2023-08-19 02:17:41 -0400

    Repository should NEVER push your build folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /users/yzhen105/Edge_Based_Reconstruction
-# Build directory: /users/yzhen105/Edge_Based_Reconstruction/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Edge_Reconst")
-subdirs("cmd")
